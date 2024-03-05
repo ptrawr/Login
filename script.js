75efcb7e49a901d81cecc7b18be7e0131d6d8810
ptrawr <162259424+ptrawr@users.noreply.github.com>
2024-03-05 10:33:39 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+function kirimData() {
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+
+  const user = ("rakha@gmail.com");
+  const pass = 99138;
+  
+  
+
+  if (user == "" ||  password == ""){
+      alert("harap isi username dan pasword")
+  }else if (password != pass ) {
+
+  }else if (username != user) {
+
+  }else {
+      alert("Login Berhasil" , document.getElementById("rakha").click());
+  }
+  const user2 = ("farhan@gmail.com");
+  const pass2 = 99790;
+  
+  
+
+  if (user == "" ||  password == ""){
+  }else if (password != pass2 ) {
+
+  }else if (username != user2 ) {
+
+  }else {
+      alert("Login Berhasil" , document.getElementById("anas").click());
+  }
+} 
